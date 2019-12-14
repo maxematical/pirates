b6304b2d205f88af3284561f1e637bc90e5d08a0
Max Battle <m4xematical@gmail.com>
2019-12-13 21:42:42 -0600

    Add collision VFX

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -7,6 +7,7 @@ public class Cannonball : MonoBehaviour
     public float Gravity { get; set; }
     public Vector3 Velocity { get; set; }
     public GameObject IgnoreCollisions;
+    public GameObject CollisionVFXPrefab;
     
     void Start()
     {
@@ -24,17 +25,31 @@ public class Cannonball : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider otherCollider)
+    void OnCollisionEnter(Collision collision)
     {
-        if (otherCollider.gameObject == IgnoreCollisions)
+        // Find ShipHealth component in the object's highest parent
+        Transform obj = collision.gameObject.transform;
+        while (obj.parent != null)
+            obj = obj.parent;
+        ShipHealth health = obj.GetComponent<ShipHealth>();
+
+        if (health?.gameObject == IgnoreCollisions)
         {
             return;
         }
-        ShipHealth health = otherCollider.GetComponent<ShipHealth>();
+
         if (health != null)
         {
             health.Health--;
             Destroy(this.gameObject);
+
+            ContactPoint contact = collision.contacts[0];
+            Vector3 vfxDirection = contact.normal;
+            vfxDirection.y = Util.Cap(vfxDirection.y, 0.2f);
+            vfxDirection.Normalize();
+
+            GameObject vfx = Instantiate(CollisionVFXPrefab, contact.point, Quaternion.identity);
+            vfx.transform.rotation = Quaternion.LookRotation(vfxDirection, Vector3.up);
         }
     }
 }
